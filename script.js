41a847d23286e7ab9281b93c768b53a766aa6f65
Will <19doylew@gmail.com>
2023-09-01 17:30:56 +1000

    script changes to run on load on github

--- script.js
+++ script.js
@@ -5,11 +5,12 @@ if(location.pathname=="/index.html"){
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop < 50) {
       document.getElementById("cover").style.scale = "1";
       document.getElementById("navBar").style.maxWidth = "100%";
-  
+      console.log('Default cover photo');
   
     } else {
       document.getElementById("cover").style.scale = "0.6";
       document.getElementById("navBar").style.maxWidth = "1000px";
+      console.log('Smaller cover photo');
       
       
     }
@@ -24,7 +25,7 @@ if(location.pathname=="/index.html"){
 function removeLink(){
 // 1. Select the elements based on their href attribute
 const elementsToRemove = document.querySelectorAll('a[href="https://elfsight.com/instagram-feed-instashow/?utm_source=websites&utm_medium=clients&utm_content=instashow&utm_term=127.0.0.1&utm_campaign=free-widget"]');
-
+ 
 // 2. Iterate through the selected elements and remove them
 elementsToRemove.forEach((element) => {
   element.remove();
